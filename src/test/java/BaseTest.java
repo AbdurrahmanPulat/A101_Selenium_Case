67e895389424c5ab1b1e3e89b8b7845e8e7dde1d
AbdurrahmanPulat <abdurrahmanpulat9@gmail.com>
2022-09-21 13:31:40 +0300

    log4j eklemesi gerçekleştirildi

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,12 +2,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(TestResultLogger.class)
+
 public class BaseTest {
 
     WebDriver driver;
